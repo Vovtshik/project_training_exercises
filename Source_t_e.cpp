c93c95e30cc5b62e5aeb40cb45f83b458070ad1e
Volodymir Konovalov <vvkon@meta.ua>
2020-05-08 13:58:57 +0300

    Update Source_t_e.cpp

--- Source_t_e.cpp
+++ Source_t_e.cpp
@@ -130,3 +130,22 @@ int main()
     a1.print();
 }
  */
+
+/* 
+class A{
+    private:
+        int a;
+    public:
+        A() :a(0) {cout << '1' << a;}
+        A(int y) :a(y) {cout << '2' << a;}
+        A(const A& x) :a(x.a) {cout << '3' << a;}
+};
+void Test()
+{
+    A a(1);
+    A *arr = &a;
+    arr = new A(a);
+
+}
+// result compilation error (undefined reference to `WinMain')
+ */
